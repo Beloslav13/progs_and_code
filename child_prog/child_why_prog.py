b7e5ff4a7e6ce81bebb36abe0869769e62355837
beloslav <velislav1329@gmail.com>
2019-11-07 22:40:32 +0300

    Format PEP8

--- child_prog/child_why_prog.py
+++ child_prog/child_why_prog.py
@@ -1,17 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+
 root = Tk()
 root.title('Виртуальный ребёнок')
 
+
 def que():
-    title = Label(root, text = 'Знакомтесь, это виртуальный ребёнок Кеша, который очень любит вопрос "Почему?"')
+    title = Label(root, text='Знакомтесь, это виртуальный ребёнок Кеша, который очень любит вопрос "Почему?"')
     title.grid(row=0)
     question = Label(root, text='Почему?')
     question.grid(row=1)
     answer = Entry()
     answer.grid(row=2)
-    btn = ttk.Button(root, text = 'Ответить', command = lambda:child(que))
+    btn = ttk.Button(root, text='Ответить', command=lambda: child(que))
     btn.grid(row=3)
 
     def child(que):
@@ -21,6 +23,7 @@ def que():
         else:
             messagebox.showerror('Неверно', 'К сожалению вы ввели не верное значение, попробуйте еще раз!')
             que()
+
 
 que()
 root.mainloop()
