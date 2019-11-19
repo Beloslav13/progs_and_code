ba0fc6f7745903d23c8f2b7b5648aa4cb56cbab8
beloslav <velislav1329@gmail.com>
2019-11-19 18:34:16 +0300

    парсер друзей vk.

---
+++ vk_parser/vk.py
@@ -0,0 +1,50 @@
+import requests
+import csv
+
+
+def take_friends():
+    """Вернуть список друзей."""
+    token = '837182c3837182c3837182c387831f94ce88371837182c3dea26888d14c55c5189a3f42'
+    version = 5.103
+    fields = 'fields'
+    user_id = '53083705'
+    response = requests.get('https://api.vk.com/method/friends.get',
+                            params={
+                                'access_token': token,
+                                'v': version,
+                                'fields': fields,
+                                'user_id': user_id,
+
+                            })
+    data = response.json()['response']['items']
+    return data
+
+
+def file_write(friends):
+    """Записать данные в файл"""
+    with open('user_friends.csv', 'w', encoding='utf-8') as file:
+        a_pen = csv.writer(file)
+        a_pen.writerow(('ID', 'Имя', 'Фамилия'))
+        for user in friends:
+            user_id = user['id']
+            first_name = user['first_name']
+            last_name = user['last_name']
+            a_pen.writerow((user_id, first_name, last_name))
+
+
+friends = take_friends()
+file_write(friends=friends)
+
+
+def print_friends(take_friends):
+    """Напечать друзей пользователя в консоле."""
+    for take_friend in take_friends:
+        user_id = take_friend['id']
+        first_name = take_friend['first_name']
+        last_name = take_friend['last_name']
+        result = '{} {}, id - {}'.format(first_name, last_name, str(user_id))
+        print(result)
+
+
+take_friends = take_friends()
+print_friends(take_friends)
