293d72850a561c3f423e89fece2e89903ecbfb33
beloslav <velislav1329@gmail.com>
2019-11-27 13:04:39 +0300

    file read

--- boock_test/files/file_reader.py
+++ boock_test/files/file_reader.py
@@ -1,8 +1,25 @@
-with open('text_files/pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents)
+filename = 'learning_python.txt'
+with open(filename, 'r', encoding='utf8') as file:
+    file_read = file.read()
+    print(file_read)
 
-filename = 'text_files/pi_digits.txt'
-with open(filename) as file_object:
-    for line in file_object:
-        print(line.rstrip())
+print('\n')
+
+with open(filename) as file:
+    python = 'Python'
+    for line in file:
+        if python in line:
+            print(line.replace(python, 'C').strip())
+        else:
+            print(line.strip())
+
+
+
+with open(filename, 'r', encoding='utf8') as file:
+    file_text = file.readlines()
+
+print('\n')
+
+
+for line in file_text:
+    print(line.strip())
